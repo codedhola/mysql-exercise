177c1abdefef0ec2d346ccd7835a1f6e8ab8e0f4
coded hola <codedhola2000@gmail.com>
2022-12-12 18:03:37 +0100

    feat: installed and configured database

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const http = require("http")
+const mysql = require("mysql")
 const {bookRoutes} = require("./routes/bookRoute")
 const {userRoutes} = require("./routes/userRoute")
+const { DB_HOST, DB_USER, DB_PASSWORD } = require("./env")
+
+const connection = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: "my_sql_exercise"
+
+})
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  
+    console.log('connected as id ' + connection.threadId);
+  });
+
 
 const server = http.createServer((req, res) => {
     
@@ -14,7 +34,15 @@ const server = http.createServer((req, res) => {
             status: "Failed",
             message: "please navigate to '/api/books' or /api/users/login''" 
         }))
-    }else{
+    }else if(req.url === "/createDB"){
+        let sql = "CREATE DATABASE my_sql_exercise"
+        connection.query(sql, (err, result) => {
+            if(err) throw error
+            console.log(result)
+            res.end("Database Created succesfully");
+        })
+    }
+    else{
         res.writeHead(404, { "Content-Type": "application/json"})
         res.end(JSON.stringify({
             status: "Failed",
