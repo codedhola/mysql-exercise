3f2d5d8d76c01257b9b87308449e6219d91f2143
coded hola <codedhola2000@gmail.com>
2022-12-17 14:56:52 +0100

    fix🐞: fix non database bug

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -5,7 +5,8 @@ const { DB_HOST, DB_USER, DB_PASSWORD } = require("../env")
   const Pool = mysql.createPool({
     host: DB_HOST,
     user: DB_USER,
-    password: DB_PASSWORD
+    password: DB_PASSWORD,
+    database: "my_sql_exercise"
   })
 
 
