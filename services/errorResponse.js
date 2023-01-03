3219a0efd8a60801198b576e310369e6bee2235d
coded hola <codedhola2000@gmail.com>
2023-01-03 23:30:33 +0100

    created a branch for postgresql exercise

--- services/errorResponse.js
+++
@@ -1,7 +0,0 @@
-module.exports = (res, err) => {
-    res.writeHead(400, { "Content-Type": "application/json"})
-       .end(JSON.stringify({ 
-            status: "Failed",
-            message: err
-        }))
-}
